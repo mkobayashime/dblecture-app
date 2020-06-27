142a622bcee771796d1ff89b49f9469537d2f9d2
Masaki Kobayashi <mkobayashime@gmail.com>
2020-06-27 17:52:09 +0900

    [Update] Style changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,11 +5,13 @@
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Poppins:wght@400;700&display=swap");
+
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Poppins", "Noto Sans JP", sans-serif;
   font-size: 16px;
-  word-spacing: 1px;
+  letter-spacing: 0.05rem;
+  color: #757575;
   -ms-text-size-adjust: 100%;
   -webkit-text-size-adjust: 100%;
   -moz-osx-font-smoothing: grayscale;
@@ -22,34 +24,6 @@ html {
 *:after {
   box-sizing: border-box;
   margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+  padding: 0;
 }
 </style>
