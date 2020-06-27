f5db9914c4c25a149b19f784e77f535c1ba357f3
Masaki Kobayashi <mkobayashime@gmail.com>
2020-06-27 19:42:18 +0900

    [Chore] Style refinements

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,22 +8,22 @@
 @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Poppins:wght@400;600&display=swap");
 
 html {
-  font-family: "Poppins", "Noto Sans JP", sans-serif;
   font-size: 16px;
   letter-spacing: 0.05rem;
-  color: #757575;
+  color: #616161;
   -ms-text-size-adjust: 100%;
   -webkit-text-size-adjust: 100%;
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
 }
 
 *,
 *:before,
 *:after {
+  font-family: "Poppins", "Noto Sans JP", sans-serif;
   box-sizing: border-box;
   margin: 0;
   padding: 0;
+  box-sizing: border-box;
 }
 </style>
