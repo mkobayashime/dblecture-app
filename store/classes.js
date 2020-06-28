71fc81e3f848b47475e959a7f83d2d235fb18144
Masaki Kobayashi <mkobayashime@gmail.com>
2020-06-28 21:07:20 +0900

    [Chore] Add array classIds/classNames

--- store/classes.js
+++ store/classes.js
@@ -1,13 +1,21 @@
 export const state = () => ({
-  classes: []
+  classes: [],
+  classIds: [],
+  classNames: []
 })
 
 export const getters = {
-  classes: (state) => state.classes
+  classes: (state) => state.classes,
+  classIds: (state) => state.classIds,
+  classNames: (state) => state.classNames
 }
 
 export const mutations = {
   update(state, data) {
     state.classes = data
+    data.forEach((item) => {
+      state.classIds.push(item.classId)
+      state.classNames.push(item.className)
+    })
   }
 }
